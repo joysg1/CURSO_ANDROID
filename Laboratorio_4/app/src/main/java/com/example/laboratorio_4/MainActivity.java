3065132b3608a77af0f833e21ec5d4213bfe4e4a
Joy <joy.nelaton@utp.ac.pa>
2024-09-02 14:24:15 -0500

    Commit_08

--- Laboratorio_4/app/src/main/java/com/example/laboratorio_4/MainActivity.java
+++ Laboratorio_4/app/src/main/java/com/example/laboratorio_4/MainActivity.java
@@ -1,8 +1,5 @@
 package com.example.laboratorio_4;
 
-import static java.lang.String.*;
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -10,19 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import java.text.MessageFormat;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnIr;     /* Declaracion del boton */
-    Button btnDeterminar;
-    TextView InEdad;
-    TextView Resultado;
+    Button btnIr; /* Declaracion del boton */
 
 
     @Override
@@ -31,9 +23,6 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         Button btnIr = (Button)findViewById(R.id.btnIr);
-        Button btnDeterminar = (Button)findViewById(R.id.btnDeterminar);
-        TextView InEdad = (TextView)findViewById(R.id.InEdad);
-        TextView Resultado = (TextView)findViewById(R.id.Resultado);
         btnIr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
@@ -45,36 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-        btnDeterminar.setOnClickListener(new View.OnClickListener(){
-             @SuppressLint("SetTextI18n")
-             @Override
-            public void onClick (View v) {
-              String edadTexto = InEdad.getText().toString();
-              if (!edadTexto.isEmpty()){
 
-                  try {
-                  int aNacimiento = Integer.parseInt(edadTexto);
-                  int aActual = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR);
-                  int edad = aActual - aNacimiento;
-                  Resultado.setText(MessageFormat.format("Edad {0}", edad));
-
-              } catch (NumberFormatException exception) {
-                   Resultado.setText("Por favor valide el dato ingresado: ");
-
-                 }
-
-             }
-
-              else {
-
-                  Resultado.setText("Por favor ingrese el dato solicitado: ");
-
-              }
-
-             }
-
-
-        });
 
 
 
