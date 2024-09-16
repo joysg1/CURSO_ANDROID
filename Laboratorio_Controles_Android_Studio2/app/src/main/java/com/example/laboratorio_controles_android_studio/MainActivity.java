bd1f2c35d462a637865aac28257017c3097bc5d7
Joy <joy.nelaton@utp.ac.pa>
2024-09-16 12:50:55 -0500

    Commit_22

--- Laboratorio_Controles_Android_Studio2/app/src/main/java/com/example/laboratorio_controles_android_studio/MainActivity.java
+++ Laboratorio_Controles_Android_Studio2/app/src/main/java/com/example/laboratorio_controles_android_studio/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.laboratorio_controles_android_studio;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +19,10 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     private Button  boton;  // Declaracion de la variable boton
     private ProgressBar progressBar; // Declaracion de la variable progressBar
+    private AutoCompleteTextView autoCompleteTextView; // Declaracion de la variable autoCompleteTextView
+    private EditText nombre; // Declaracion de las variables editText
+    private EditText edad;
+    private EditText carrera;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,19 +33,40 @@ public class MainActivity extends AppCompatActivity {
         progressBar = findViewById(R.id.progressBar); // Asociacion del id con la variable progressBar
         progressBar.setProgress(25); // Establecemos el porcentaje de progreso al 25%
 
+        autoCompleteTextView = findViewById(R.id.editTextFacultad); // Asociacion del id con la variable autoCompleteTextView
+
+        String[] facultades = {"Sistemas", "Civil", "Industrial", "Eléctrica", "Ciencias y Tecnología", "Mecánica"}; // Array para las sugerencias de facultades
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.select_dialog_item, facultades);
+        autoCompleteTextView.setAdapter(adapter);
+
+        nombre = findViewById(R.id.editTextNombre); // Asociacion de los ids con el editText
+        edad = findViewById(R.id.editTextEdad);
+        carrera = findViewById(R.id.editTextCarrera);
+
         boton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
+                if (validarCampos()) {
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Por favor, llene todos los campos", Toast.LENGTH_SHORT).show();
+                }
             }
         });
-
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private boolean validarCampos() {
+        return !autoCompleteTextView.getText().toString().isEmpty() &&
+                !nombre.getText().toString().isEmpty() &&
+                !edad.getText().toString().isEmpty() &&
+                !carrera.getText().toString().isEmpty();
     }
 }
