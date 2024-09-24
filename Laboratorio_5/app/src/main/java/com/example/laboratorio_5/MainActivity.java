5bcb12d83dbad3e4ce8ff27ad06c0de9a69fdda6
Joy <joy.nelaton@utp.ac.pa>
2024-09-24 16:01:17 -0500

    Commit_32

--- Laboratorio_5/app/src/main/java/com/example/laboratorio_5/MainActivity.java
+++ Laboratorio_5/app/src/main/java/com/example/laboratorio_5/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.laboratorio_5;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,25 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button botonSiguiente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        botonSiguiente = findViewById(R.id.botonSiguiente);
+
+
+        botonSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
