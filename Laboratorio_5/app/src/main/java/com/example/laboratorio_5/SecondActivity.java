4060ead2bb908f3253bfca833dc50f926070ffcb
Joy <joy.nelaton@utp.ac.pa>
2024-09-24 16:18:23 -0500

    Commit_33

--- Laboratorio_5/app/src/main/java/com/example/laboratorio_5/SecondActivity.java
+++ Laboratorio_5/app/src/main/java/com/example/laboratorio_5/SecondActivity.java
@@ -1,6 +1,9 @@
 package com.example.laboratorio_5;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,39 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SecondActivity extends AppCompatActivity {
-
+    private Button botonSiguiente2;
+    private Button botonAnterior2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_second);
+
+        botonSiguiente2 = findViewById(R.id.botonSiguiente2);
+
+        botonSiguiente2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        botonAnterior2 = findViewById(R.id.botonAnterior2);
+
+        botonAnterior2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- Laboratorio_5/app/src/main/java/com/example/laboratorio_5/ThirdActivity.java
+++ Laboratorio_5/app/src/main/java/com/example/laboratorio_5/ThirdActivity.java
@@ -1,6 +1,9 @@
 package com.example.laboratorio_5;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ThirdActivity extends AppCompatActivity {
-
+    private Button botonAnterior3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_third);
+
+
+        botonAnterior3 = findViewById(R.id.botonAnterior3);
+
+        botonAnterior3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ThirdActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
 }
