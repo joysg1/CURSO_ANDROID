eaf277e0e1f3092698545c5edc859ab77f01640b
Joy <joy.nelaton@utp.ac.pa>
2024-09-26 11:54:08 -0500

    Commit_40

--- Laboratorio_5/app/src/main/java/com/example/laboratorio_5/WebActivity.java
+++ Laboratorio_5/app/src/main/java/com/example/laboratorio_5/WebActivity.java
@@ -1,6 +1,10 @@
 package com.example.laboratorio_5;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -10,12 +14,24 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class WebActivity extends AppCompatActivity {
-
+    private Button botonCerrar; // Declaracion de variables asociadas al boton y webview
+    private WebView Webview1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_web);
+
+        botonCerrar = findViewById(R.id.botonCerrar);  // Identificamos las variables con los id
+        Webview1 = findViewById(R.id.WebView1);
+        Webview1.setWebViewClient(new WebViewClient()); // Agregar funcionalidad de navegación web dentro del app
+
+        botonCerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        }); // Se vuelve al activity anterior al presionar el boton cerrar
 
 
 
